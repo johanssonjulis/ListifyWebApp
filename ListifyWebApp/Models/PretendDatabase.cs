6aedbcbf4d2b3335af6cc2169b4236679fa60ed8
Olivia5353 <115546755+Olivia5353@users.noreply.github.com>
2023-04-27 14:10:10 +0200

    Fortsättning fejtk db

--- ListifyWebApp/Models/PretendDatabase.cs
+++ ListifyWebApp/Models/PretendDatabase.cs
@@ -2,5 +2,18 @@
 {
     public class PretendDatabase
     {
+        static List<Listify> ListTable = new List<Listify>();
+
+        public List<Listify> GetList()
+        {
+            return ListTable;
+        }
+        public void AddList(Listify listify)
+        {
+            ListTable.Add(listify);
+        }
+
+
+
     }
 }
--- ListifyWebApp/Pages/Page2.cshtml.cs
+++ ListifyWebApp/Pages/Page2.cshtml.cs
@@ -1,12 +1,30 @@
+using ListifyWebApp.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ListifyWebApp.Pages
 {
+
+    
     public class Page2Model : PageModel
     {
+        PretendDatabase db;
+
+        public Page2Model(PretendDatabase db)
+        {
+            this.db = db;
+        }
+
         public void OnGet()
         {
+        }
+        [BindProperty]
+        public Listify listify { get; set; }
+
+        public IActionResult OnPost()
+        {
+            db.AddList(listify);
+            return RedirectToPage("Page3");
         }
     }
 }
