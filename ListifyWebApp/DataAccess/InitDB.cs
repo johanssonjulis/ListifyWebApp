bb0baea9c8f834938f69f76d35b22152d8664846
johanssonjulis <johansson.julis@gmail.com>
2023-04-27 14:41:00 +0200

    DB

--- ListifyWebApp/DataAccess/InitDB.cs
+++ ListifyWebApp/DataAccess/InitDB.cs
@@ -1,4 +1,6 @@
-﻿namespace ListifyWebApp.DataAccess
+﻿using Microsoft.Azure.Cosmos;
+
+namespace ListifyWebApp.DataAccess
 {
     public class InitDB
     {
@@ -6,9 +8,20 @@
 
         public void Init() 
         {
+            string endpointUri = "https://localhost:7277/"; // Där ligger min CosmosDB-server
+            string primaryKey = "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw=="; // Den är den hemliga nyckeln som authentiserar mig mot servern
+
+            CosmosClient client = new CosmosClient(endpointUri, primaryKey); // Här skapar jag en klient, som kan "prata" med databasen.
+
+            string databaseName = "ListifyDB";
+            string containerName = "Listify";
+
+            DatabaseResponse database = client.CreateDatabaseIfNotExistsAsync(databaseName).GetAwaiter().GetResult(); // skapar databasen om den inte finns, synkront (i samma tråd som resten av applikationen kör)
+
+            var container = database.Database.CreateContainerIfNotExistsAsync(containerName, "/partitionKey").GetAwaiter().GetResult(); // exakt samma sak för containern
 
 
         }
-        
+
     }
 }
