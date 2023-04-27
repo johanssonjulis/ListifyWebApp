ca35e920db9746de9435482fabe0b86c585b2c23
Debbie <debbie.grothen@gmail.com>
2023-04-27 13:08:15 +0200

    Get and Post

---
+++ ListifyClient/ListifyClient/DataContent.cs
@@ -0,0 +1,6 @@
+﻿namespace ListifyClient
+{
+    internal class DataContent
+    {
+    }
+}
--- ListifyClient/ListifyClient/RequestHandler.cs
+++ ListifyClient/ListifyClient/RequestHandler.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,6 +21,43 @@ namespace ListifyClient
             int choice = int.Parse(Console.ReadLine());
             Console.Clear();
             Console.WriteLine($"You wrote {choice}");
+        }
+        public void Get()
+        {
+            HttpClient httpClient = new HttpClient();
+            Uri uri = new Uri("https://localhost:7104/api/Product");
+
+            // For Post, I want to send in something called HttpContent.
+            // I create an object that will accept the variables I want
+            // to send. In this case, just a string.
+            DataContent dataContent = new DataContent("Anything else!");
+
+            // JsonConvert is from Newtonsoft.Json, a package one can
+            // install. It is well liked. An alternative for JsonSerializer.Serialize.
+            string json = JsonConvert.SerializeObject(dataContent);
+
+            Console.WriteLine("Json sent: " + json);
+
+            
+            StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            
+            HttpResponseMessage response = httpClient.PostAsync(uri, content).Result;
+            Console.WriteLine("Status Code: " + (int)response.StatusCode);
+            Console.WriteLine("Means: " + response.StatusCode);
+
+        }
+        public void Post() 
+        {
+            
+        }
+        public void Put() 
+        {
+        
+        }
+        public void Delete() 
+        {
+
         }
     }
 }
