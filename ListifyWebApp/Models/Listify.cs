682815b5fdbeb3c3b4383b1b73298d7f2cd1f1ea
Debbie <debbie.grothen@gmail.com>
2023-05-02 16:13:35 +0200

    changed name - Task to TaskItem

--- ListifyWebApp/Models/Listify.cs
+++ ListifyWebApp/Models/Listify.cs
@@ -4,17 +4,17 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public List<Task> tasks { get; set; }
+        public List<ItemTask> tasks { get; set; }
 
         public Listify(int id, string name)
         {
             Id = id;
             Name = name;
-            tasks = new List<Task>();
+            tasks = new List<ItemTask>();
         }
         public Listify() 
         { 
-            tasks = new List<Task>();
+            tasks = new List<ItemTask>();
         }
     }
 }
--- ListifyWebApp/wwwroot/js/site.js
+++ ListifyWebApp/wwwroot/js/site.js
@@ -3,7 +3,21 @@
 
 // Write your JavaScript code.
 const submitButton = document.getElementById("submitButton");
+const placeholderForTasks = document.getElementById("placeholderForTasks");
+
 
 submitButton.addEventListener("click", function (event) {
-    
+    event.preventDefault();
+    submitButton.setAttribute("hidden", "true");
+    const test = document.createElement("input");
+    test.setAttribute("type", "text");
+    placeholderForTasks.appendChild(test);
+    const addTask = document.createElement("button");
+    addTask.textContent = "Add task";
+    placeholderForTasks.appendChild(addTask);
+});
+addTask.addEventListener("click", function (event) {
+    event.preventDefault();
+    test.value = "";
+   
 })
