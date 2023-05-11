84a1ec84a3fd121c56288ce6e21d03245c11c553
Debbie <debbie.grothen@gmail.com>
2023-05-11 23:01:39 +0200

    shadows added

--- ListifyWebApp/wwwroot/js/site.js
+++ ListifyWebApp/wwwroot/js/site.js
@@ -8,11 +8,13 @@ const placeHolder = document.getElementById("placeholderForTasks");
 addTaskButton.addEventListener("click", function (event) {
     event.preventDefault();
     let container = document.createElement("div");
+    container.setAttribute("class", "inputForTask")
   
     let nextinput = document.createElement("input");
     nextinput.setAttribute("name", "task");
+    nextinput.setAttribute("placeholder", "Task: ");
+    
     placeHolder.append(container);
-   
     container.appendChild(nextinput);
 
 });
